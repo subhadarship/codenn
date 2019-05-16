202acaebad6de1ffb5353e11367e39299c1a2e46
subhadarship <35211168+subhadarship@users.noreply.github.com>
2019-05-16 17:18:22 -0400

    Update buildData.sh

--- src/model/buildData.sh
+++ src/model/buildData.sh
@@ -13,11 +13,14 @@ fi
 # Prepare C# and SQL data
 SQL_UNK_THRESHOLD=3
 CSHARP_UNK_THRESHOLD=2
+PYTHON_UNK_THRESHOLD=2
 NL_UNK_THRESHOLD=2
 
-python buildData.py sql $MAX_CODE_LENGTH $MAX_NL_LENGTH $SQL_UNK_THRESHOLD $NL_UNK_THRESHOLD
-python buildData.py csharp $MAX_CODE_LENGTH $MAX_NL_LENGTH $CSHARP_UNK_THRESHOLD $NL_UNK_THRESHOLD
+# python buildData.py sql $MAX_CODE_LENGTH $MAX_NL_LENGTH $SQL_UNK_THRESHOLD $NL_UNK_THRESHOLD
+# python buildData.py csharp $MAX_CODE_LENGTH $MAX_NL_LENGTH $CSHARP_UNK_THRESHOLD $NL_UNK_THRESHOLD
+python buildData.py python $MAX_CODE_LENGTH $MAX_NL_LENGTH $PYTHON_UNK_THRESHOLD $NL_UNK_THRESHOLD
 
 
-th buildData.lua -language sql -max_code_length $MAX_CODE_LENGTH -max_nl_length $MAX_NL_LENGTH -batch_size $BATCH_SIZE
-th buildData.lua -language csharp -max_code_length $MAX_CODE_LENGTH -max_nl_length $MAX_NL_LENGTH -batch_size $BATCH_SIZE
+# th buildData.lua -language sql -max_code_length $MAX_CODE_LENGTH -max_nl_length $MAX_NL_LENGTH -batch_size $BATCH_SIZE
+# th buildData.lua -language csharp -max_code_length $MAX_CODE_LENGTH -max_nl_length $MAX_NL_LENGTH -batch_size $BATCH_SIZE
+th buildData.lua -language python -max_code_length $MAX_CODE_LENGTH -max_nl_length $MAX_NL_LENGTH -batch_size $BATCH_SIZE
