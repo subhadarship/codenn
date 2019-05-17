c7d785fb222fc7447c665beb6d367829ebcba095
subhadarship <35211168+subhadarship@users.noreply.github.com>
2019-05-16 20:19:17 -0400

    Update run.sh

--- src/model/run.sh
+++ src/model/run.sh
@@ -12,5 +12,5 @@ BATCH_SIZE=100
 th main.lua -dev_ref_file $CODENN_DIR/data/stackoverflow/${LANGUAGE}/dev/ref.txt -gpuidx $GPUIDX -language $1
 
 # Run prediction
-th predict.lua -encoder ${LANGUAGE}.encoder -decoder ${LANGUAGE}.decoder -beamsize $BEAMSIZE -gpuidx $GPUIDX -language ${LANGUAGE} \
+th predict.lua -encoder ${CODENN_WORK}/${LANGUAGE}.encoder -decoder ${CODENN_WORK}/${LANGUAGE}.decoder -beamsize $BEAMSIZE -gpuidx $GPUIDX -language ${LANGUAGE} \
   -batch_size $BATCH_SIZE -max_code_length $MAX_CODE_LENGTH -max_nl_length $MAX_NL_LENGTH
